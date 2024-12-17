21389be9e3e30f23ba648c2c642ac2fe995232c0
hetesaus <mhs@live.nl>
2024-12-17 23:06:27 +0100

    Fixed styling?

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const domain = "hetesaus.github.io";
 
-// Function to get query parameters from the URL
 function getQueryParam(param) {
 	const urlParams = new URLSearchParams(window.location.search);
 	return urlParams.get(param);
 }
 
-// Get the channel name from the query parameter
 const channelName = getQueryParam("channel");
 
-// Update channel name if provided
 if (channelName) {
 	const player = document.getElementById("player");
 	const chat = document.getElementById("chat");
